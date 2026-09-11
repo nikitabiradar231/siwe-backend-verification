f7e0350ada2ea56ca775ad3949b2bb086da97288
Nikita Biradar <nikitabiradar300@gmail.com>
2026-09-11 23:58:14 +0530

    feat: initialize Kitaab Bazaar project structure and configuration

---
+++ next.config.mjs
@@ -0,0 +1,22 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.externals.push('pino-pretty', 'lokijs', 'encoding');
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      '@x402/evm/upto/client': false,
+      '@x402/evm/exact/client': false,
+      '@x402/core/client': false,
+      '@x402/svm/exact/client': false,
+      '@x402/evm': false,
+      '@react-native-async-storage/async-storage': false,
+      fs: false,
+      net: false,
+      tls: false,
+    };
+    return config;
+  },
+};
+
+export default nextConfig;
